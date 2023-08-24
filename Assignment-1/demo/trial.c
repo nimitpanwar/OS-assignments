fea0affacc0ab624726b184d4a4a7f3909920e64
Nimit Panwar <71833404+nejim3h@users.noreply.github.com>
2023-08-24 18:50:59 +0530

    Update trial.c

--- Assignment-1/demo/trial.c
+++ Assignment-1/demo/trial.c
@@ -1,86 +1,95 @@
-#include "loader2.h"
+#include "loader.h"
 
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
 int fd;
 
-void print_memory_content(const void *memory, ssize_t size) {
-    const unsigned char *bytes = (const unsigned char *)memory;
-    
-    for (size_t i = 0; i <size; ++i) {
-        printf("%02X ", bytes[i]); // Print each byte as a hexadecimal value
-        if ((i + 1) % 16 == 0) {
-            printf("\n"); // Print a new line after every 16 bytes
-        }
-    }
-    printf("\n");
-}
-
-
 /*
  * release memory and other cleanups
  */
 void loader_cleanup() {
+  
 }
 
 /*
  * Load and run the ELF executable file
  */
 void load_and_run_elf(char** exe) {
-}
-    
-int main(){    
-    fd = open("fib2", O_RDONLY);
-    off_t fileSize = lseek(fd, 0, SEEK_END);
-    void *content = malloc(fileSize);
+    // Step 1: Open and read the ELF binary file
+    int fd = open(exe[1], O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    off_t ELF_size = lseek(fd, 0, SEEK_END);
     lseek(fd, 0, SEEK_SET);
-    ssize_t bytesread= read(fd, content, fileSize);
-    // print_memory_content(content, fileSize);
-    ehdr = (Elf32_Ehdr *)content;
-    unsigned char *fileData = (unsigned char *)content;
-    memcpy(ehdr,fileData,sizeof(Elf32_Ehdr));
-    
-    // printf("%04X\n", ehdr->e_ident[0]);
-    // printf("fileData: %p\n", (void *)fileData);
+
+    void *content = malloc(ELF_size);
+    ssize_t bytesread = read(fd, content, ELF_size);
+    close(fd);
+
+    // Step 2: Parse ELF header and program header table
+    Elf32_Ehdr *ehdr = (Elf32_Ehdr *)content;
+    unsigned char *ELF_data = (unsigned char *)content;
+
+    size_t numEntries = ehdr->e_phnum;
     off_t phOffset = ehdr->e_phoff;
     size_t phEntrySize = ehdr->e_phentsize;
-    size_t numPhEntries = ehdr->e_phnum;
-    phdr = (Elf32_Phdr *)malloc(phEntrySize*numPhEntries);
-    ssize_t ph_read = pread(fd, phdr, phEntrySize*numPhEntries, phOffset);
-    // for (size_t i = 0; i < 2; ++i) {
-    //     printf("p_type=%08X, p_vaddr=%08X\n", phdr[i].p_type , phdr[i].p_memsz);
-    // // }
 
-    Elf32_Phdr *entry_phdr = NULL;
-    for (size_t i = 0; i < numPhEntries; ++i) {
+    Elf32_Phdr *phdr = (Elf32_Phdr *)malloc(phEntrySize * numEntries);
+    fd = open(exe[1], O_RDONLY); // Reopen the file to read program headers
+    ssize_t ph_read = pread(fd, phdr, phEntrySize * numEntries, phOffset);
+    close(fd);
+
+    // Step 3: Locate and allocate memory for the loadable segment
+    Elf32_Phdr *reqPhdr = NULL;
+    for (size_t i = 0; i < numEntries; ++i) {
         if (phdr[i].p_type == PT_LOAD && ehdr->e_entry >= phdr[i].p_vaddr && ehdr->e_entry < phdr[i].p_vaddr + phdr[i].p_memsz) {
-            if(phdr[i].p_type!=PT_NULL){
-                entry_phdr=&phdr[i];
-                break;
-            }
+            reqPhdr = &phdr[i];
+            break;
         }
     }
-    // printf("p_offset=%08X, p_vaddr=%08X\n",phdr[i].p_offset,phdr[i].p_vaddr);
 
-    // void* source_addr = (void*)((uintptr_t)fileData + phdr[i].p_offset);
-    void *source_addr = (void *)((uintptr_t)fileData + (entry_phdr->p_offset));
+    if (reqPhdr == NULL) {
+        fprintf(stderr, "Required program header not found\n");
+        exit(EXIT_FAILURE);
+    }
 
-    // printf("source address: %p\n", (void *)source_addr);
-    void *virtual_mem;
-    virtual_mem=mmap(NULL,entry_phdr->p_memsz,PROT_READ|PROT_WRITE|PROT_EXEC,MAP_ANONYMOUS|MAP_PRIVATE,0,0);
+    void *reqAddr = ELF_data + reqPhdr->p_offset;
+
+    // Allocate memory with proper protection flags using mmap
+    void *virtual_mem = mmap(NULL, reqPhdr->p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
     if (virtual_mem == MAP_FAILED) {
         perror("mmap");
         exit(EXIT_FAILURE);
     }
 
-    
-    memcpy(virtual_mem, source_addr, entry_phdr->p_filesz);
+    // Copy segment content into allocated memory
+    memcpy(virtual_mem, reqAddr, reqPhdr->p_filesz);
 
-
+    // Step 4: Resolve entry point function and execute it
     typedef int (*StartFunction)(void);
-    StartFunction _start = (StartFunction)(virtual_mem+(ehdr->e_entry-entry_phdr->p_vaddr));
+    StartFunction _start = (StartFunction)(virtual_mem + (ehdr->e_entry - reqPhdr->p_vaddr));
     int result = _start();
-    printf("_start return value = %d\n", result);
 
-    // munmap(virtual_mem, phdr[i].p_memsz);
-}   
+    // Step 5: Clean up allocated memory and resources
+    printf("_start return value = %d\n", result);
+    munmap(virtual_mem, reqPhdr->p_memsz);
+    free(content);
+    free(phdr);
+}
+
+int main(int argc, char** argv) 
+{
+  if(argc != 2) {
+    printf("Usage: %s <ELF Executable> \n",argv[0]);
+    exit(1);
+  }
+  // 1. carry out necessary checks on the input ELF file
+  // 2. passing it to the loader for carrying out the loading/execution
+  load_and_run_elf(argv);
+  // 3. invoke the cleanup routine inside the loader  
+  loader_cleanup();
+  return 0;
+} 
