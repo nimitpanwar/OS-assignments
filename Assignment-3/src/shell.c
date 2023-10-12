1df83c46e4529babaa2bdfdd4f36d1b4cc737f6d
samyakm38 <142526536+samyakm38@users.noreply.github.com>
2023-10-12 19:42:45 +0530

    Update shell.c

--- Assignment-3/src/shell.c
+++ Assignment-3/src/shell.c
@@ -3,33 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <sys/types.h>    
+#include <sys/wait.h>  
 #include <string.h>
 #include <time.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
 int NCPU;
 int TSLICE;
 
-#define MAX_PROCESSES 100
 
-struct Process {
-    int pid;
-    char pname[100];
-};
+typedef struct  {
+    int array[100];
+    int front;
+    int rear;
+    sem_t mutex;
+}queue;
 
-struct Process processes[MAX_PROCESSES]; //array containing processes;
-int process_count = 0; // stores processes present currently
 
-void custom_signal_handler(int signum) {
-    if (signum == SIGUSR1) {
-        if (process_count > 0) {
-            // Move the child process to the ready queue or take any other desired action
-        } else {
-            printf("No processes in the array to move to the ready queue.\n");
-        }
-    }
-}
 
 struct cmnd_Elt {
     char command[1024];
@@ -47,6 +40,8 @@ static void my_handler(int signum) {
         ctrl_clicked = 1;
     }
 }
+
+
 
 int read_user_input(char* input, int size, char* command, char** arguments) {
     fgets(input, size, stdin);
@@ -72,139 +67,122 @@ int read_user_input(char* input, int size, char* command, char** arguments) {
     return 0;
 }
 
-int create_process_and_run(char* command, char** arguments) {
+void enqueue (queue* q,int p){
+    q->rear++;
+    q->array[q->rear]=p;
+}
+
+
+void create_process_for_scheduler(queue* ready_queue, char* executable) {
+    int length = strlen(executable);
+
+    // Check if the length is less than 3 (minimum for "./x")
+    if (length < 3) {
+        printf("Invalid input: %s\n", executable);
+        return;
+    }
+
+    char argument[length - 1];
+    int j = 0;
+
+    // Skip the first two characters (typically "./")
+    for (int i = 2; i < length; i++) {
+        argument[j] = executable[i];
+        j++;
+    }
+    argument[j] = '\0';
+
     int child_PID = fork();
     if (child_PID < 0) {
         printf("Something went wrong.\n");
         exit(10);
     } else if (child_PID == 0) {
-        if (execvp(command, arguments) == -1) {
-            perror("execvp");
+        int my_pid= getpid();
+        kill(my_pid,SIGSTOP);
+        if (execlp(executable, argument, NULL) == -1) {
+            perror("execlp");
             exit(10);
         }
     } else {
-        int ret;
-        time_t startTime;
-        time(&startTime);
-        waitpid(child_PID, &ret, 0);
-        time_t endTime;
-        time(&endTime);
-        if (WIFEXITED(ret)) {
-            cmnd_Array[cmnd_count - 1].execution_time = difftime(endTime, startTime);
-            cmnd_Array[cmnd_count - 1].pid = child_PID;
-            printf("%d Exit = %d\n", child_PID, WEXITSTATUS(ret));
-        } else {
-            printf("Abnormal termination of %d\n", child_PID);
-        }
+
+        sem_wait(&ready_queue->mutex);
+        enqueue(ready_queue, child_PID);
+        // printf("%d\n",child_PID);
+        sem_post(&ready_queue->mutex);
+
     }
-    return 0;
 }
+
+
 
 int launch(char* command, char** arguments) {
     int status;
-    status = create_process_and_run(command, arguments);
+    // status = create_process_and_run(command, arguments);
     return status;
 }
 
-void print_History() {
-    for (int i = 0; i < cmnd_count; i++) {
-        printf("%d- %s \n", i + 1, cmnd_Array[i].command);
-    }
-}
 
-void print_On_Exit() {
-    printf("\nNo. : Command    PID    Start Time   Execution Time\n");
-    for (int i = 0; i < cmnd_count; i++) {
-        printf("%d : %s    %d    %s    %.2lf seconds\n", i + 1,
-               cmnd_Array[i].command, cmnd_Array[i].pid,
-               asctime(localtime(&cmnd_Array[i].start_time)),
-               cmnd_Array[i].execution_time);
-    }
-}
-
-void print_processes() {
-    printf("Processes Array:\n");
-    for (int i = 0; i < process_count; i++) {
-        printf("PID: %d, pname: %s\n", processes[i].pid, processes[i].pname);
-    }
-}
-
-void shell_Loop() {
+void shell_Loop(queue* ready_queue) {
     char input[1024];
     char command[1024];
     char* arguments[1024];
-    do {
-        printf(">>> $ ");
-        fflush(stdout);
-        int sig_received = read_user_input(input, sizeof(input), command, arguments);
-        if (sig_received) {
-            print_On_Exit();
-            return;
-        }
-        if (strcmp(command, "history") == 0) {
-            print_History();
-        }// Inside the "submit" command handler
+    char cNCPU[3];
+    char cTSLICE[4];
 
-        // Inside the "submit" command handler
-        if (strcmp(command, "submit") == 0) {
-            // Check for valid input and arguments
-            if (arguments[1] != NULL) {
-                if (process_count < MAX_PROCESSES) {
-                    int child_PID = fork();
-                    if (child_PID < 0) {
-                        printf("Something went wrong.\n");
-                        exit(10);
-                    } else if (child_PID == 0) {
-                        // Child process
-                        // Use the custom_signal_handler function declared above
-                        signal(SIGUSR1, custom_signal_handler);
+    if(fork()==0){                           
+        snprintf(cNCPU, sizeof(cNCPU), "%d", NCPU);
+        snprintf(cTSLICE, sizeof(cTSLICE), "%d", TSLICE);              
+        execlp("./simpleScheduler", "simpleScheduler",cNCPU, cTSLICE,NULL);
+    }
+    else{
+        do {
+            printf(">>> $ ");
+            fflush(stdout);
+            int sig_received = read_user_input(input, sizeof(input), command, arguments);
+            if (sig_received) {
+                shm_unlink("shared memory");
+                return;
+            }
+            if (strcmp(command, "history") == 0) {
+            }
 
-                        // Sleep or pause to wait for the signal from the scheduler
-                        pause();
+            if (strcmp(command, "submit") == 0) {
+                if (arguments[1] != NULL) {
+                    if (ready_queue->rear< 100-1) {
+                        create_process_for_scheduler(ready_queue,arguments[1]);
                     } else {
-                        // Parent process
-                        struct Process new_process;
-                        new_process.pid = child_PID;
-                        strcpy(new_process.pname, arguments[1]);
-                        processes[process_count] = new_process;
-                        process_count++;
-
-                        // Send the signal only if the array was previously empty
-                        if (process_count == 1) {
-                        // Trigger the signal (e.g., SIGUSR1)
-                            kill(child_PID, SIGUSR1);
-                        }
+                        printf("Maximum process limit reached.\n");
                     }
                 } else {
-                    printf("Maximum process limit reached.\n");
+                    printf("Usage: submit <pname>\n");
                 }
-            } else {
-                printf("Usage: submit <pname>\n");
+            }else {
+                launch(command, arguments);
             }
-        }else if (strcmp(command, "print_processes") == 0) {
-            // Call the function to print the processes array
-            print_processes();
-        }else {
-            launch(command, arguments);
-        }
-    } while (1);
+        } while (1);
+    }
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <integer1> <integer2>\n", argv[0]);
-        return 1;
-    }
-
-    NCPU = atoi(argv[1]);
-    TSLICE = atoi(argv[2]);
+int main(int argv, char**argc) {
+    
+    NCPU = atoi(argc[1]);  
+    TSLICE = atoi(argc[2]);  
 
     struct sigaction sig;
     memset(&sig, 0, sizeof(sig));
     sig.sa_handler = my_handler;
     sigaction(SIGINT, &sig, NULL);
 
-    shell_Loop();
+    int fd = shm_open("shared memory", O_CREAT | O_RDWR, 0666);
+    ftruncate(fd, sizeof(queue));
+
+    queue* ready_queue = mmap(0, sizeof(queue), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    sem_init(&ready_queue->mutex, 1, 1);
+
+    ready_queue->front=-1;
+    ready_queue->rear=-1;
+    shell_Loop(ready_queue);
 
     return 0;
 }
