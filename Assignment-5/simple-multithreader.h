5334c522d400435926b8a58b7c76fdcbcd35f3f6
samyakm38 <142526536+samyakm38@users.noreply.github.com>
2023-11-10 18:50:43 +0530

    Update simple-multithreader.h

--- Assignment-5/simple-multithreader.h
+++ Assignment-5/simple-multithreader.h
@@ -3,9 +3,7 @@
 #include <functional>
 #include <stdlib.h>
 #include <cstring>
-#include <pthread.h>
-#include <vector>
-#include <chrono>
+
 
 int user_main(int argc, char **argv);
 
@@ -13,90 +11,122 @@ void demonstration(std::function<void()> && lambda) {
   lambda();
 }
 
-#define NUM_THREADS 4
 
-struct ThreadData {
+int count=0;
+
+void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numThreads){
+
+  struct timespec startTime, endTime;
+  clock_gettime(CLOCK_MONOTONIC, &startTime);
+
+
+  int chunk = (high-low)/(numThreads);
+  pthread_t tid[numThreads];
+
+  struct thread_args {
     int low;
     int high;
-    std::function<void(int)> lambda;
-    ThreadData(int low, int high, std::function<void(int)> lambda) : low(low), high(high), lambda(lambda) {}
-};
+    std::function<void(int)> lambdafunc;
+    thread_args(int low, int high, std::function<void(int)> lambda) : low(low), high(high), lambdafunc(lambda) {}
+  };
 
-struct ThreadData2D {
+  for (int i = 0; i < numThreads; i++) {
+    thread_args* to_pass = new thread_args((i*chunk),((i+1)*chunk),lambda);
+    pthread_create(&tid[i], NULL, [](void* arg) -> void* {
+        thread_args* curr_arg = (thread_args*)(arg);
+        for (int j = curr_arg->low; j < curr_arg->high; j++) {
+            curr_arg->lambdafunc(j);
+        }
+        return NULL;
+        delete curr_arg;
+    }, (void*)to_pass);
+  }
+
+  for (int i = 0; i < numThreads; i++) {
+    pthread_join(tid[i], NULL);
+  }
+
+  clock_gettime(CLOCK_MONOTONIC, &endTime);
+
+  double seconds = (endTime.tv_sec - startTime.tv_sec) + (endTime.tv_nsec - startTime.tv_nsec) / 1e9;
+
+  printf("Execution Time: %f seconds\n", seconds);
+  
+} 
+
+
+
+void parallel_for(int low1, int high1, int low2, int high2,std::function<void(int, int)> &&lambda, int numThreads){
+
+  struct timespec startTime, endTime;
+  clock_gettime(CLOCK_MONOTONIC, &startTime);
+
+
+  pthread_t tid[numThreads];
+
+  struct thread_args2 {
     int low1;
     int high1;
     int low2;
     int high2;
-    std::function<void(int, int)> lambda;
-    ThreadData2D(int low1, int high1, int low2, int high2, std::function<void(int, int)> lambda) : low1(low1), high1(high1), low2(low2), high2(high2), lambda(lambda) {}
-};
+    std::function<void(int,int)> lambdafunc;
+    thread_args2(int low1, int high1, int low2, int high2, std::function<void(int,int)> lambdafn) : low1(low1), high1(high1), low2(low2), high2(high2), lambdafunc(lambdafn) {}
+  };
 
-void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numThreads) {
-    std::vector<pthread_t> threads(numThreads);
-    int range = high - low;
-    auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < numThreads; ++i) {
-        pthread_create(&threads[i], NULL, [](void* arg) -> void* {
-            auto data = static_cast<ThreadData*>(arg);
-            for (int j = data->low; j < data->high; ++j) {
-                data->lambda(j);
-            }
-            delete data;
-            return NULL;
-        }, new ThreadData{i * range / numThreads + low, (i + 1) * range / numThreads + low, lambda});
-    }
-    for (auto& thread : threads) {
-        pthread_join(thread, NULL);
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end-start;
-    std::cout << "Execution time: " << diff.count() << " s\n";
+  int chunk1 = (high1- low1)/numThreads;
+  int chunk2=  (high2- low2)/numThreads;
+
+  for(int i=0;i<numThreads;i++){
+    thread_args2* to_pass = new thread_args2((i*chunk1),(i+1)*chunk1,low2,high2,lambda);
+    pthread_create(&tid[i], NULL, [](void* arg) -> void*{ 
+      thread_args2* curr_arg = (thread_args2*)(arg);
+      for(int j=curr_arg->low1;j<curr_arg->high1;j++){
+        for(int k=curr_arg->low2;k<curr_arg->high2;k++){
+          curr_arg->lambdafunc(j,k);
+        }
+      }
+      delete curr_arg;
+      return NULL;
+    },(void*)to_pass);
+  }
+  
+  for (int i = 0; i < numThreads; i++) {
+    pthread_join(tid[i], NULL);
+  } 
+
+  clock_gettime(CLOCK_MONOTONIC, &endTime);
+
+  double seconds = (endTime.tv_sec - startTime.tv_sec) + (endTime.tv_nsec - startTime.tv_nsec) / 1e9;
+
+  printf("Execution Time: %f seconds\n", seconds);
+
+
 }
 
-void parallel_for(int low, int high, std::function<void(int)> &&lambda) {
-    parallel_for(low, high, std::move(lambda), NUM_THREADS);
-}
 
-void parallel_for(int low1, int high1, int low2, int high2, std::function<void(int, int)> &&lambda, int numThreads) {
-    std::vector<pthread_t> threads(numThreads);
-    int range1 = high1 - low1;
-    int range2 = high2 - low2;
-    auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < numThreads; ++i) {
-        pthread_create(&threads[i], NULL, [](void* arg) -> void* {
-            auto data = static_cast<ThreadData2D*>(arg);
-            for (int j = data->low1; j < data->high1; ++j) {
-                for (int k = data->low2; k < data->high2; ++k) {
-                    data->lambda(j, k);
-                }
-            }
-            delete data;
-            return NULL;
-        }, new ThreadData2D{i * range1 / numThreads + low1, (i + 1) * range1 / numThreads + low1, low2, high2, lambda});
-    }
-    for (auto& thread : threads) {
-        pthread_join(thread, NULL);
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end-start;
-    std::cout << "Execution time: " << diff.count() << " s\n";
-}
 
 int main(int argc, char **argv) {
+
   int x=5,y=1;
+
   auto lambda1 = [x, &y](void) {
     y = 5;
     std::cout<<"====== Welcome to Assignment-"<<y<<" of the CSE231(A) ======\n";
   };
-  demonstration(lambda1);
 
-  int rc = user_main(argc, argv);
- 
+  demonstration(lambda1); 
+
   auto lambda2 = []() {
     std::cout<<"====== Hope you enjoyed CSE231(A) ======\n";
+
   };
+
+  int rc = user_main(argc, argv);
+
   demonstration(lambda2);
   return rc;
 }
 
 #define main user_main
+
+
