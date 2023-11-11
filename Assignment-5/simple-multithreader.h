1dfe3336ee3fc48931cccb32e0a3e44b9b116938
Nimit Panwar <71833404+nejim3h@users.noreply.github.com>
2023-11-11 14:33:08 +0530

    Update simple-multithreader.h

--- Assignment-5/simple-multithreader.h
+++ Assignment-5/simple-multithreader.h
@@ -3,62 +3,85 @@
 #include <functional>
 #include <stdlib.h>
 #include <cstring>
-
+#include <errno.h>
 
 int user_main(int argc, char **argv);
 
 void demonstration(std::function<void()> && lambda) {
   lambda();
 }
-
-
+// Function to execute a lambda function in parallel for a range of integers
 void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numThreads){
+    // Get the start time
+    struct timespec startTime, endTime;
+    if (clock_gettime(CLOCK_MONOTONIC, &startTime) != 0) {
+        perror("Failed to get start time");
+    return;
+}
 
-  struct timespec startTime, endTime;
-  clock_gettime(CLOCK_MONOTONIC, &startTime);
+// Calculate the chunk size for each thread
+int chunk = (high-low)/(numThreads);
+pthread_t tid[numThreads];
 
-
-  int chunk = (high-low)/(numThreads);
-  pthread_t tid[numThreads];
-
-  struct thread_args {
+// Structure to hold arguments for each thread
+struct thread_args {
     int low;
     int high;
     std::function<void(int)> lambdafunc;
     thread_args(int low, int high, std::function<void(int)> lambda) : low(low), high(high), lambdafunc(lambda) {}
   };
 
-  for (int i = 0; i < numThreads; i++) {
+// Creating Threads
+for (int i = 0; i < numThreads; i++) {
+    // Allocate memory for thread arguments
     thread_args* to_pass = new thread_args((i*(high-low)/(numThreads))+low,((i+1)*(high-low)/(numThreads))+low,lambda);
-    pthread_create(&tid[i], NULL, [](void* arg) -> void* {
+    if (pthread_create(&tid[i], NULL, [](void* arg) -> void* {  // Create a new thread
         thread_args* curr_arg = (thread_args*)(arg);
+        // Execute the lambda function for the range of integers
         for (int j = curr_arg->low; j < curr_arg->high; j++) {
             curr_arg->lambdafunc(j);
         }
+        delete curr_arg; // Free the memory allocated for thread arguments
         return NULL;
-        delete curr_arg;
-    }, (void*)to_pass);
+    }, (void*)to_pass) != 0) {
+      perror("Failed to create thread");
+      delete to_pass;
+      return;
+    }
+}
+
+// Wait for all threads to finish
+for (int i = 0; i < numThreads; i++) {
+    if (pthread_join(tid[i], NULL) != 0) {
+      perror("Failed to join thread");
+      return;
+    }
   }
 
-  for (int i = 0; i < numThreads; i++) {
-    pthread_join(tid[i], NULL);
+// Get the end time for calculation of execution time
+if (clock_gettime(CLOCK_MONOTONIC, &endTime) != 0) {
+    perror("Failed to get end time");
+    return;
   }
-
-  clock_gettime(CLOCK_MONOTONIC, &endTime);
-
+  // Calculate and print the execution time
   double seconds = (endTime.tv_sec - startTime.tv_sec) + (endTime.tv_nsec - startTime.tv_nsec) / 1e9;
-
   printf("Execution Time: %f seconds\n", seconds);
   
 } 
 
-
+/*
+*
+* Similar to the above function, but for two ranges of integers.
+*
+*/
 
 void parallel_for(int low1, int high1, int low2, int high2,std::function<void(int, int)> &&lambda, int numThreads){
 
-  struct timespec startTime, endTime;
-  clock_gettime(CLOCK_MONOTONIC, &startTime);
-
+struct timespec startTime, endTime;
+  if (clock_gettime(CLOCK_MONOTONIC, &startTime) != 0) {
+    perror("Failed to get start time");
+    return;
+  }
 
   pthread_t tid[numThreads];
 
@@ -73,7 +96,7 @@ void parallel_for(int low1, int high1, int low2, int high2,std::function<void(in
 
   for(int i=0;i<numThreads;i++){
     thread_args2* to_pass = new thread_args2(i * (high1-low1) / numThreads , (i + 1) * (high1-low1) / numThreads ,low2,high2,lambda);
-    pthread_create(&tid[i], NULL, [](void* arg) -> void*{ 
+    if (pthread_create(&tid[i], NULL, [](void* arg) -> void*{ 
       thread_args2* curr_arg = (thread_args2*)(arg);
       for(int j=curr_arg->low1;j<curr_arg->high1;j++){
         for(int k=curr_arg->low2;k<curr_arg->high2;k++){
@@ -82,46 +105,43 @@ void parallel_for(int low1, int high1, int low2, int high2,std::function<void(in
       }
       delete curr_arg;
       return NULL;
-    },(void*)to_pass);
+    },(void*)to_pass) != 0) {
+      perror("Failed to create thread");
+      delete to_pass;
+      return;
+    }
   }
   
   for (int i = 0; i < numThreads; i++) {
-    pthread_join(tid[i], NULL);
+    if (pthread_join(tid[i], NULL) != 0) {
+      perror("Failed to join thread");
+      return;
+    }
   } 
 
-  clock_gettime(CLOCK_MONOTONIC, &endTime);
-
+  if (clock_gettime(CLOCK_MONOTONIC, &endTime) != 0) {
+    perror("Failed to get end time");
+    return;
+  }
   double seconds = (endTime.tv_sec - startTime.tv_sec) + (endTime.tv_nsec - startTime.tv_nsec) / 1e9;
-
   printf("Execution Time: %f seconds\n", seconds);
-
-
 }
 
-
-
 int main(int argc, char **argv) {
-
   int x=5,y=1;
-
   auto lambda1 = [x, &y](void) {
     y = 5;
     std::cout<<"====== Welcome to Assignment-"<<y<<" of the CSE231(A) ======\n";
   };
 
   demonstration(lambda1); 
-
   auto lambda2 = []() {
     std::cout<<"====== Hope you enjoyed CSE231(A) ======\n";
-
   };
 
   int rc = user_main(argc, argv);
-
   demonstration(lambda2);
   return rc;
 }
 
 #define main user_main
-
-
